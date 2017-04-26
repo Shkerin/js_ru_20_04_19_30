1c8b5e20151844401c19cc7e408df75603a77ef2
Vladimir Shkerin <vladshkerin@gmail.com>
2017-04-26 18:21:50 +0300

    add component Comment

---
+++ src/Comment.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+export default class Comment extends Component {
+    state = {
+        isOpen: false
+    };
+
+    render() {
+        const {comment} = this.props;
+        return (
+            <blockquote>
+                {this.getUser()}
+                {this.getBody()}
+            </blockquote>
+        )
+    }
+
+    getUser() {
+        const text = this.state.isOpen ? "open" : "close";
+        return <h2 onClick={this.toggleOpen}>{this.props.comment.user} ({text})</h2>
+    }
+
+    getBody() {
+        return this.state.isOpen &&
+            <quotient>
+                {this.props.comment.text}
+            </quotient>;
+    }
+
+    toggleOpen = ev => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+}
