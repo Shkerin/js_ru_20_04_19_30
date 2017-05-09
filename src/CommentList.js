c56449e60d30bb8ddab9a8798b0d7820ef494608
Vladimir Shkerin <vladshkerin@gmail.com>
2017-05-09 21:37:25 +0300

    fix home tast 01

--- src/CommentList.js
+++ src/CommentList.js
@@ -2,20 +2,40 @@
 // который можно открывать/закрывать по клику на соответствующую ссылку
 // (текст ссылкы должен меняться)
 
-import React from 'react';
-import Comment from './Comment';
+import React, {Component} from 'react'
+import Comment from './Comment'
 
-export default function CommentList({comments}) {
-    if (comments === undefined) {
-        return <ul></ul>;
+export default class CommentList extends Component {
+    state = {
+        isOpen: false
+    };
+
+    render() {
+        const linkText = this.state.isOpen ? 'hide comments' : 'show comments';
+
+        return (
+            <div>
+                <a href="#" onClick={this.toggleOpen}>{linkText}</a>
+                {this.getBody()}
+            </div>
+        )
     }
 
-    const elements = comments.map(
-        comment => <li key={comment.id}><Comment comment={comment}/></li>
-    );
-    return (
-        <ul>
-            {elements}
-        </ul>
-    )
+    getBody() {
+        if (!this.state.isOpen) return null;
+        const {comments} = this.props;
+        if (!comments || !comments.length) return <p>No comments yet</p>;
+        return (
+            <ul>
+                {comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)}
+            </ul>
+        )
+    }
+
+    toggleOpen = ev => {
+        ev.preventDefault();
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
 }
